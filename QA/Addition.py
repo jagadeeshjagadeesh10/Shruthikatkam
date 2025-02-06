d33e39ba10bc6cf328a7f67d2253bd990ed48821
shruthikatkam04@gmail.com <shruthikatkam04@gmail.com>
2025-02-05 23:41:01 -0600

    sathish first program

---
+++ QA/Addition.py
@@ -0,0 +1,11 @@
+# Get input from the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Get input from the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers together
+sum = num1 + num2
+
+# Print the result
+print("The sum is:", sum)
